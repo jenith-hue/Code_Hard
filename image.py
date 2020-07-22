15ec81903b1bed9c21690f715e35cca9f09857e9
Jenith <52031340+jenith-hue@users.noreply.github.com>
2020-07-22 14:37:57 +0530

    2nd Phase Coding
    2nd Phase Coding

--- image.py
+++ image.py
@@ -15,4 +15,28 @@ weightsPath = "./yolov3.weights"
 net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
 
 image =cv2.imread('./images/test_image.jpg')
-
+(H, W) = image.shape[:2]
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),swapRB=True, crop=False)
+net.setInput(blob)
+start = time.time()
+layerOutputs = net.forward(ln)
+end = time.time()
+print("Frame Prediction Time : {:.6f} seconds".format(end - start))
+boxes = []
+confidences = []
+classIDs = []
+for output in layerOutputs:
+    for detection in output:
+        scores = detection[5:]
+        classID = np.argmax(scores)
+        confidence = scores[classID]
+        if confidence > 0.5 and classID == 0:
+            box = detection[0:4] * np.array([W, H, W, H])
+            (centerX, centerY, width, height) = box.astype("int")
+            x = int(centerX - (width / 2))
+            y = int(centerY - (height / 2))
+            boxes.append([x, y, int(width), int(height)])
+            confidences.append(float(confidence))
+            classIDs.append(classID)
